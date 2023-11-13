81cd905e0d79f759e669c9e7686a3f62094e9528
razikhaNawaz <razikhafaheem02@gmail.com>
2023-11-13 22:37:22 +0530

    todoapp

---
+++ src/components/Todos.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Todos = () => {
+  return (
+    <div>
+        <ul>
+            <li>Learn react</li>
+            <li>Learn typescript</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Todos
