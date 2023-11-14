6cdf1dcc6f17c150fb32903d3a947074b44dacf0
razikhaNawaz <razikhafaheem02@gmail.com>
2023-11-14 12:21:09 +0530

    createdModelprop

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,17 @@ import React from 'react';
 
 import './App.css';
 import Todos from './components/Todos';
+import Todo from './models/todo';
 
 function App() {
+  const todos=[
+    new Todo('Learn React'),
+    new Todo('Learn Typescript'),
+    new Todo('Learn framer motion')
+  ]
   return (
     <div>
-      <Todos items={['Learn react', 'Learn typescript' ]} />
+      <Todos items={todos} />
     </div>
   );
 }
